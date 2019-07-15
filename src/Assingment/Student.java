f73e4c903665720d4e0e2e5dcc9134824e962df8
kazungu <kazungu61@gmail.com>
2019-07-15 13:02:36 +0300

    initial commit

---
+++ src/Assingment/Student.java
@@ -0,0 +1,11 @@
+package Assingment;
+
+public class Student {
+    String name = "John";
+    int roll_no = 2;
+    public static void main(String[] args) {
+        Student object1 = new Student();
+        System.out.println("Name: "+ object1.name);
+        System.out.println("Roll number: "+ object1.roll_no);
+    }
+}
